f8e58f0e4eda04ef37e4c5189ff09728a9b28a6c
JulioLlerena1 <juceller@espol.edu.ec>
2026-06-27 19:53:06 -0500

    Impresion de la tabla de clasificacion

--- SimuladordeMundial.asm
+++ SimuladordeMundial.asm
@@ -1,7 +1,20 @@
 .data
-    # Mensaje entrada
+    # Mensajes entrada
     msg_titulo:     .asciiz "Simulador de Fase de Grupos del Mundial\n"
     msg_ingreso:    .asciiz "Ingrese el nombre del pais: "
+    
+    msg_tabla_antes:    .asciiz "\n=============================================\n  TABLA DE POSICIONES (DESORDENADA)\n============================================="
+    msg_tabla_despues:  .asciiz "\n=============================================\n  TABLA DE POSICIONES (ORDENADA)\n============================================="
+    
+    # Cabecera de la tabla y espaciadores de formato
+    msg_header:         .asciiz "\nPos  Pais                 PTS  GF  GC  DG\n---------------------------------------------\n"
+    sp_pos:             .asciiz "    "   # 4 espacios
+    sp_pts:             .asciiz " "      # 1 espacio
+    sp_gf:              .asciiz "    "   # 4 espacios
+    sp_gc:              .asciiz "   "    # 3 espacios
+    sp_dg:              .asciiz "   "    # 3 espacios
+    
+    .align 2
     
     # Arreglos paralelos
     nombres:        .space 80       # 4 equipos de 20 bytes cada uno
@@ -29,13 +42,23 @@ main:
     # Llamar a funciones
     jal FaseIngreso
     jal FasePartidos
+    
+    li $v0, 4
+    la $a0, msg_tabla_antes
+    syscall
+    jal MostrarTabla
+    
     jal FaseBubbleSort
+    
+    li $v0, 4
+    la $a0, msg_tabla_despues
+    syscall
+    jal MostrarTabla
+    
     jal FaseClasificacion
 
     li $v0, 10
     syscall
-
-
 
 FaseIngreso:
     li $t0, 0               
@@ -51,16 +74,130 @@ loop_ingreso:
     # Leer string
     li $v0, 8
     move $a0, $t1           
-    li $a1, 20              # Max 20 caracteres
+    li $a1, 20
     syscall
 
-    addi $t1, $t1, 20       # Avanzar al siguiente "slot" de 20 bytes
+    addi $t1, $t1, 20       # Avanzar al siguiente slot de 20 bytes
     addi $t0, $t0, 1
     j loop_ingreso
 
 fin_ingreso:
     jr $ra
 
+
+MostrarTabla:
+    addi $sp, $sp, -4
+    sw $ra, 0($sp)
+
+    li $v0, 4
+    la $a0, msg_header
+    syscall
+
+    li $t0, 0
+    
+loop_tabla:
+    beq $t0, 4, fin_tabla
+
+    # Calcular offsets
+    sll $t8, $t0, 2
+    
+    li $t7, 20
+    mult $t0, $t7
+    mflo $t1                # $t1 = i * 20
+    la $t2, nombres
+    add $t1, $t2, $t1       # $t1 = Dirección base del nombre del equipo actual
+
+    # 1. Imprimir número de Posición (i + 1)
+    addi $a0, $t0, 1
+    li $v0, 1
+    syscall
+
+    li $v0, 4
+    la $a0, sp_pos
+    syscall
+
+    # 2. Imprimir Nombre con ancho fijo (20 caracteres) limpiando el '\n'
+    move $t3, $t1           # $t3 = Puntero al carácter actual de la cadena
+    li $t4, 0               # $t4 = Contador de caracteres impresos
+    
+print_char_loop:
+
+    lb $t5, 0($t3)
+    beq $t5, $zero, pad_spaces
+    
+    li $t7, 10
+    beq $t5, $t7, pad_spaces
+    
+    # Imprimir el carácter válido
+    li $v0, 11
+    move $a0, $t5
+    syscall
+    
+    addi $t3, $t3, 1
+    addi $t4, $t4, 1
+    j print_char_loop
+
+pad_spaces:
+
+    li $t7, 20
+    bge $t4, $t7, fin_pad
+    li $v0, 11
+    li $a0, 32
+    syscall
+    addi $t4, $t4, 1
+    j pad_spaces
+    
+fin_pad:
+
+    # Espacio antes de la columna PTS para alineación exacta
+    li $v0, 4
+    la $a0, sp_pts
+    syscall
+
+    # 3. Imprimir PTS
+    lw $a0, puntos($t8)
+    li $v0, 1
+    syscall
+    
+    li $v0, 4
+    la $a0, sp_gf
+    syscall
+
+    # 4. Imprimir GF
+    lw $a0, goles_favor($t8)
+    li $v0, 1
+    syscall
+    
+    li $v0, 4
+    la $a0, sp_gc
+    syscall
+
+    # 5. Imprimir GC
+    lw $a0, goles_contra($t8)
+    li $v0, 1
+    syscall
+    
+    li $v0, 4
+    la $a0, sp_dg
+    syscall
+
+    # 6. Imprimir DG
+    lw $a0, dif_goles($t8)
+    li $v0, 1
+    syscall
+
+    li $v0, 4
+    la $a0, nl
+    syscall
+
+    addi $t0, $t0, 1
+    
+    j loop_tabla
+
+fin_tabla:
+    lw $ra, 0($sp)
+    addi $sp, $sp, 4
+    jr $ra
 
 
 FasePartidos:
@@ -151,10 +288,11 @@ Jugar_Partido:
     sub $t4, $t2, $t3
     sw $t4, dif_goles($t9)
 
-    # --- Lógica de Puntos FIFA ---
+    # Lógica de Puntos FIFA
     # Si A > B -> A gana (3 pts)
     # Si B > A -> B gana (3 pts)
     # Si A == B -> Empate (1 pt cada uno)
+    
     beq $t0, $t1, Empate
     bgt $t0, $t1, GanaA
 
@@ -200,25 +338,30 @@ loopJ:
     # 1. Comparar puntos
     lw $t3, puntos($s0)
     lw $t4, puntos($s1)
+    
     blt $t3, $t4, do_swap   # Si puntos(j) < puntos(j+1), intercambiar
     bgt $t3, $t4, no_swap
 
     # 2. Desempate
     lw $t3, dif_goles($s0)
     lw $t4, dif_goles($s1)
+    
     blt $t3, $t4, do_swap
     bgt $t3, $t4, no_swap
 
     # 3. Desempate
     lw $t3, goles_favor($s0)
     lw $t4, goles_favor($s1)
+    
     blt $t3, $t4, do_swap
     j no_swap               # Si todo es igual, no intercambiar
 
 do_swap:
 
-    lw $t8, puntos($s0); lw $t9, puntos($s1)
-    sw $t9, puntos($s0); sw $t8, puntos($s1)
+    lw $t8, puntos($s0)
+    lw $t9, puntos($s1)
+    sw $t9, puntos($s0)
+    sw $t8, puntos($s1)
 
 
     lw $t8, goles_favor($s0)
@@ -261,7 +404,7 @@ no_swap:
     blt $t1, $t2, loopJ
 
     addi $t0, $t0, 1
-    j LoopI
+    j loopI
 
 end_sort:
 
